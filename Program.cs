a0dd29263f2ebbb8463a865d32358b5be551e211
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-19 08:51:12 +0200

    Implement Call Methods From the .NET Class Library Using C# section

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Random dice = new Random();
+//int roll1 = dice.Next();
+//int roll2 = dice.Next(101);
+//int roll3 = dice.Next(50, 101);
+
+//Console.WriteLine($"The first roll: {roll1}");
+//Console.WriteLine($"The second roll: {roll2}");
+//Console.WriteLine($"The third roll: {roll3}");
+
+
+int firstValue = 700;
+int secondValue = 1000;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
+
+
+

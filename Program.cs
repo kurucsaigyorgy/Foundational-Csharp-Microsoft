7b5c7071ddd2caa9686dcd60323ef48eee6a7c0a
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-19 10:55:06 +0200

    Implement bonus point logic for rolling doubles or triple; winning conditions

--- Program.cs
+++ Program.cs
@@ -9,12 +9,51 @@
 //Console.WriteLine($"The third roll: {roll3}");
 
 
-int firstValue = 700;
+/*int firstValue = 700;
 int secondValue = 1000;
 int largerValue;
 
 largerValue = Math.Max(firstValue, secondValue);
 Console.WriteLine(largerValue);
+*/
+
+//--------------------------------------------------------------------------
+
+// Dice rolling mini game
+// Add Decision Logic to Your Code Using if, else, and else if statements in C#
+
+using System.Globalization;
+
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (roll1 == roll2 && roll2 == roll3)
+{
+    Console.WriteLine("You rolled triples! +6 points to total.");
+}
+else if (roll1 == roll2 || roll1 == roll3 || roll2 == roll3)
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total.");
+    total += 2;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You Win!");
+}
+else
+{
+    Console.WriteLine("Sorry, you lose.");
+}
+
+
+//--------------------------------------------------------------------------
 
 
 

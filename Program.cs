2e0754f9043f793441a74f4289dd80833a6378fe
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-25 16:05:29 +0200

    Implement Create Readable Code with Conventions, Whitespace, and Comments in C# section

--- Program.cs
+++ Program.cs
@@ -123,16 +123,138 @@ Console.WriteLine(largerValue);
 //    Console.WriteLine(name);
 //}
 
+//string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+//foreach (var orderID in orderIDs)
+//{
+//    if (orderID.StartsWith("B"))
+//    {
+//        Console.WriteLine(orderID);
+//    }
+//}
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
-//Complete a challenge activity for nested iteration and selection statements
+//Create Readable Code with Conventions, Whitespace, and Comments in C#
 
-string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-foreach (var orderID in orderIDs)
+
+/*
+string fisrstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{fisrstName} purchased {widgetsPurchased} widgets");
+*/
+//--------------------------------------------------------------------------
+
+/*
+Random random = new Random();
+string[] orderIDs = new string[5];
+//Loop through each blank orderID
+for (int letter = 0; letter < orderIDs.Length; letter++)
 {
-    if (orderID.StartsWith("B"))
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and the suffix together, then assign to current OrderID
+    orderIDs[letter] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderId in orderIDs)
+{
+    Console.WriteLine(orderId);
+}
+*/
+//--------------------------------------------------------------------------
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+
+/*
+Random random = new Random();
+string[] orderIDs = new string[5];
+for (int letter = 0; letter < orderIDs.Length; letter++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[letter] = prefix + suffix;
+}
+foreach (var orderId in orderIDs)
+{
+    Console.WriteLine(orderId);
+}
+*/
+
+//--------------------------------------------------------------------------
+
+// Example 1:
+using System.Globalization;
+/*
+Console
+.
+WriteLine
+(
+"Hello Example 1!"
+)
+;
+
+// Example 2:
+string firstWord="Hello";string lastWord = "Example 2"; Console.WriteLine(firstWord+" "+lastWord+"!"); 
+*/
+
+//--------------------------------------------------------------------------
+/*
+Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+{
+    if (roll1 == roll2 && roll2 == roll3)
     {
-        Console.WriteLine(orderID);
+        Console.WriteLine("You rolled triples! +6 bonus points to total!");
+        total += 6;
+    } 
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus points to total!");
+        total += 2;
     }
 }
+
+*///--------------------------------------------------------------------------
+
+/*
+    This code sample reverses a given string and counts the occurences of a given character,
+    then prints the results to the console window.
+*/
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message) 
+{ 
+    if (letter == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
+
+string newmessage = new String(message);
+
+Console.WriteLine(newmessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
